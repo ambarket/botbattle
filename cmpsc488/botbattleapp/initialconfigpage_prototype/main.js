58cdaa1702ca9b9a45eaec0b1381a3d72c88228d
ambarket <ambarket@gmail.com>
2015-02-17 23:02:16 -0800

    Setting up to work on initialconfiguration stuff

---
+++ cmpsc488/botbattleapp/initialconfigpage_prototype/main.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var session = require('express-session');
+var https = require('https');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var app = express();
+var sessionStore = session({secret: 'sssh!', cookie: { maxAge: 600000}, resave: true, saveUninitialized: true });
+
+app.use(bodyParser.json() );
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(sessionStore);
+
+// Log every incomming request, then pass along for futher processing
+app.use(function( req, res, next) {
+    console.log('%s %s', req.method, req.url);
+    next();
+});
+
+var options = {
+   key: fs.readFileSync('server.key'),
+   cert: fs.readFileSync('server.crt')
+};
+
+var server = https.createServer(options, app).listen(6058);
+
+var io = require('socket.io').listen(server);
+
+var runExtern = require('../multiclient_prototype/runExtern');
+runExtern(app, io);
