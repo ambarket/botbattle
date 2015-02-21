f294efd8535d1a1161c5b8034a5a7d790ee56256
Stevenpc3 <Stevenpc3@live.com>
2015-02-20 23:19:55 -0500

    Create FileManager.js with partial outline

---
+++ cmpsc488/botbattleapp/nodejs_applications/FileManager.js
@@ -0,0 +1,25 @@
+/**
+* Provides a local file management. This class will implement application specific
+* insert, find, update, and delete methods to abstract away the lower level file management from the rest
+* of the application logic
+* @class FileMangaer
+* @constructor 
+*/
+
+module.exports = function FileManager() {
+    // Private variables
+    var multer = require('multer');
+    var self = this;
+    
+    /* 
+     *  create file
+     *  create folder
+     *  read file
+     *  read folder
+     *  perhaps relate funtions to users instead of just set or get
+    // Create Game Modules Directory
+    // Create Private Tournament Directory
+    // Create Public Tournaments Directory
+    // Create Test Arenas Tmp Directory 
+     */
+};
