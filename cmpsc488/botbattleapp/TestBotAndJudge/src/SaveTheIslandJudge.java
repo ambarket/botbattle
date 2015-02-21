6a570774a14b5a22c4c153825a683a52e605b0bb
Randall Hudson <Hudsonrj09@gmail.com>
2015-02-21 17:57:04 -0500

    implement interface

--- cmpsc488/botbattleapp/TestBotAndJudge/src/SaveTheIslandJudge.java
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/SaveTheIslandJudge.java
@@ -1,4 +1,11 @@
 
-public class SaveTheIslandJudge {
+public class SaveTheIslandJudge implements Player {
+	String username;
 
+	@Override
+	public String getMove(String board) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
