ca092ec919b450dc2d0154faf1da384adfdeae3f
Randall Hudson <Hudsonrj09@gmail.com>
2015-02-21 19:00:05 -0500

    Created game.java to implement game interface

---
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/Game.java
@@ -0,0 +1,54 @@
+package game.manger;
+
+/**
+ * @author Randall Hudson
+ *
+ */
+public class Game implements GameInterface {
+
+	/* (non-Javadoc)
+	 * @see game.manger.GameInterface#getBoard()
+	 */
+	@Override
+	public String getBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see game.manger.GameInterface#updateBoard(java.lang.String)
+	 */
+	@Override
+	public void updateBoard(String move) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see game.manger.GameInterface#isValidMove(java.lang.String)
+	 */
+	@Override
+	public boolean isValidMove(String move) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see game.manger.GameInterface#isGameOver()
+	 */
+	@Override
+	public boolean isGameOver() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see game.manger.GameInterface#getHTMLForBoard()
+	 */
+	@Override
+	public String getHTMLForBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
