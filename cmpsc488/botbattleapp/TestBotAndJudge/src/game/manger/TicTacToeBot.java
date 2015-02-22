50e58666adacf97ca398c35711a989ad3dad066f
Randall Hudson <Hudsonrj09@gmail.com>
2015-02-21 21:03:14 -0500

    Implemented simple getMove method

---
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/TicTacToeBot.java
@@ -0,0 +1,33 @@
+package game.manger;
+
+public class TicTacToeBot implements Player {
+
+	boolean temp;
+	
+	public TicTacToeBot(boolean temp) {
+		this.temp = temp;
+	}
+	
+	@Override
+	public String getMove(String board) {
+		int row = 1, col = 1;
+		int i = 0;
+		for (char c : board.toCharArray()) {
+			if(c == '0'){
+				String move = row + ", " + col + ", " + (temp ? "X" : "O");
+				return move;
+			}
+			
+			col++;
+			if(col == 4){
+				col = 1;
+				row++;
+			}
+		}
+		
+		
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
