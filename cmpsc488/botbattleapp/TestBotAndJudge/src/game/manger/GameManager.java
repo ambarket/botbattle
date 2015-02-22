bc2b682fe144c695eea1ee33a1d425312bc36751
Randall Hudson <Hudsonrj09@gmail.com>
2015-02-22 16:19:08 -0500

    Added game results to game manager object

--- cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/GameManager.java
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/GameManager.java
@@ -5,6 +5,7 @@ public class GameManager implements Runnable {
 	private Player player1;
 	private Player player2;
 	private GameInterface game;
+	private GameResults results;
 	
 	
 	
@@ -17,6 +18,7 @@ public class GameManager implements Runnable {
 		this.player1 = player1;
 		this.player2 = player2;
 		this.game = game;
+		results = new GameResults();
 	}
 
 
@@ -25,15 +27,22 @@ public class GameManager implements Runnable {
 	public void run() {
 		int i = 0;
 		String move = "";
+		results.addBoard(game.getBoard());
 		while(!game.isGameOver()){
 			
-			if(i % 2 == 0)
+			if(i % 2 == 0){
 				move = player1.getMove(game.getBoard());
-			else
+				results.addMove(move, 1);
+			}				
+			else{
 				move = player2.getMove(game.getBoard());
+				results.addMove(move, 2);
+			}				
 
-			if(game.isValidMove(move))
+			if(game.isValidMove(move)){
 				game.updateBoard(move);
+				results.addBoard(game.getBoard());
+			}				
 			else{
 				System.out.println("Invalid move made");
 				break;
