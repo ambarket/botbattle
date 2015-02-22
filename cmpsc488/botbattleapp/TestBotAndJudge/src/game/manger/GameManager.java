fba64fded6094954aba95f0cbcd33418b2e90c22
Randall Hudson <Hudsonrj09@gmail.com>
2015-02-22 15:49:14 -0500

    added missing fields to game manager

--- cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/GameManager.java
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/GameManager.java
@@ -2,10 +2,44 @@ package game.manger;
 
 public class GameManager implements Runnable {
 
+	private Player player1;
+	private Player player2;
+	private GameInterface game;
+	
+	
+	
+	/**
+	 * @param player1
+	 * @param player2
+	 * @param game
+	 */
+	public GameManager(Player player1, Player player2, GameInterface game) {
+		this.player1 = player1;
+		this.player2 = player2;
+		this.game = game;
+	}
+
+
+
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		int i = 0;
+		String move = "";
+		while(!game.isGameOver()){
+			
+			if(i % 2 == 0)
+				move = player1.getMove(game.getBoard());
+			else
+				move = player2.getMove(game.getBoard());
+
+			if(game.isValidMove(move))
+				game.updateBoard(move);
+			else{
+				System.out.println("Invalid move made");
+				break;
+			}
+			i++;
+		}
 	}
 
 }
