91bea300f3007c83ca7d7c053c678d31023a902c
Randall Hudson <Hudsonrj09@gmail.com>
2015-02-21 21:06:33 -0500

    Simple test with tic tac toe is up and running

--- cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/testDriver.java
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/testDriver.java
@@ -11,7 +11,29 @@ public class testDriver {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		TicTacToeGame game = new TicTacToeGame();
+		TicTacToeBot bot1 = new TicTacToeBot(false);
+		TicTacToeBot bot2 = new TicTacToeBot(true);
+		
+		int i = 0;
+		String move = "";
+		while(!game.isGameOver()){
+			
+			if(i % 2 == 0)
+				move = bot1.getMove(game.getBoard());
+			else
+				move = bot2.getMove(game.getBoard());
 
+			if(game.isValidMove(move))
+				game.updateBoard(move);
+			else{
+				System.out.println("Invalid move made");
+				break;
+			}
+			i++;
+		}
+		
+		System.out.println(game.getBoard());
 	}
 
 }
