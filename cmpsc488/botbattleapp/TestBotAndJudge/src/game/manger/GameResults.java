585147220270fd3c91af3d89fd1d7770374924fb
Randall Hudson <Hudsonrj09@gmail.com>
2015-02-22 16:03:03 -0500

    Added fields and functions to game results object

--- cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/GameResults.java
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/GameResults.java
@@ -1,5 +1,29 @@
 package game.manger;
 
-public class GameResults {
+import java.util.ArrayList;
+import java.util.List;
 
+public class GameResults {
+	private List<String> boards;
+	private List<String> player1Moves;
+	private List<String> player2Moves;
+	private int winner;
+	
+	public GameResults() {
+		boards = new ArrayList<String>();
+		player1Moves = new ArrayList<String>();
+		player2Moves = new ArrayList<String>();
+		winner = -1; //TODO use enumeration
+	}
+	
+	public void addMove(String move, int player){
+		if(player == 1)
+			player1Moves.add(move);
+		else //(player == 2) //TODO change player to enum and maybe fix this if
+			player2Moves.add(move);
+	}
+	
+	public void addBoard(String board){
+		boards.add(board);
+	}
 }
