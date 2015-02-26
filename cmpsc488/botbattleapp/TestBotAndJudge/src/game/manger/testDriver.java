236eb7779fa3a0d71df1d8f14aad6b68495d347f
Randall Hudson <Hudsonrj09@gmail.com>
2015-02-25 23:02:30 -0500

    commenting out old code

--- cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/testDriver.java
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/testDriver.java
@@ -11,28 +11,28 @@ public class testDriver {
 	 */
 	public static void main(String[] args) {
 		
-		testHumanVersusBot();
+		//testHumanVersusBot();
 	}
 	
-	public static void testBotPlayers(){
-		TicTacToeGame game = new TicTacToeGame();
-		TicTacToeBot bot1 = new TicTacToeBot(false);
-		TicTacToeBot bot2 = new TicTacToeBot(true);
-		
-		GameManager foo = new GameManager(bot1, bot2, game);
-		
-		foo.run();
-		System.out.println(game.getBoard());
-	}
+//	public static void testBotPlayers(){
+//		TicTacToeGame game = new TicTacToeGame();
+//		TicTacToeBot bot1 = new TicTacToeBot(false);
+//		TicTacToeBot bot2 = new TicTacToeBot(true);
+//		
+//		GameManager foo = new GameManager(bot1, bot2, game);
+//		
+//		foo.run();
+//		System.out.println(game.getBoard());
+//	}
 	
-	public static void testHumanVersusBot(){
-		TicTacToeGame game = new TicTacToeGame();
-		TicTacToeBot bot1 = new TicTacToeBot(false);
-		HumanPlayer palyer2 = new HumanPlayer();
-		
-		GameManager foo = new GameManager(bot1, palyer2, game);
-		
-		foo.run();
-		System.out.println(game.getBoard());
-	}
+//	public static void testHumanVersusBot(){
+//		TicTacToeGame game = new TicTacToeGame();
+//		TicTacToeBot bot1 = new TicTacToeBot(false);
+//		HumanPlayer palyer2 = new HumanPlayer();
+//		
+//		GameManager foo = new GameManager(bot1, palyer2, game);
+//		
+//		foo.run();
+//		System.out.println(game.getBoard());
+//	}
 }
