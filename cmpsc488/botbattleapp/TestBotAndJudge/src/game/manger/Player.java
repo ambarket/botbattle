437bd712f44f3e39cc9e1096169005b646266d1c
Randall Hudson <Hudsonrj09@gmail.com>
2015-02-25 22:59:20 -0500

    added buffered readers and writers to communicate with bot process

--- cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/Player.java
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/Player.java
@@ -1,8 +1,13 @@
 package game.manger;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 /**
  * @author Randall
@@ -12,31 +17,49 @@ public class Player {
 	private String botFilePath;
 	private Process botProcess;
 	private String usersName;
+	private BufferedReader reader;
+	private BufferedWriter writer;
 	
+	
+	//TODO: change thrown exception to try catch block or
+	//		maybe add try catch block in gameManager
 	/**
 	 * @param botFilePath
 	 * @param botProcess
 	 * @param usersName
+	 * @throws IOException 
 	 */
-	public Player(String botFilePath, String usersName) {
+	public Player(String botFilePath, String usersName) throws IOException {
 		this.botFilePath = botFilePath;
 		this.usersName = usersName;
 		
-		try {
-			this.botProcess = Runtime.getRuntime().exec("java " + botFilePath + "/" + usersName);
-		} catch (IOException e) {
-			this.botProcess = null;
-		}
+		ProcessBuilder builder = new ProcessBuilder("java", usersName);
+		builder.directory(new File(botFilePath));
+		
+		botProcess = builder.start();
+		
+		OutputStream stdin = botProcess.getOutputStream(); 
+        InputStream stdout = botProcess.getInputStream();
+
+        reader = new BufferedReader(new InputStreamReader(stdout));
+        writer = new BufferedWriter(new OutputStreamWriter(stdin));
 	}
 	
-	public String getBotFilePath() {
-		return botFilePath;
+	public String getMove(String board){
+		
+		try {
+			writer.write(board);
+			return reader.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 
-	public String getUsersName() {
-		return usersName;
-	}
-	
+	//TODO: determine if these Streams are needed outside of this class
+	//		if they are not then remove these getters
 	public OutputStream getOutputStream(){
 		if(botProcess != null)
 			return botProcess.getOutputStream();
@@ -51,6 +74,14 @@ public class Player {
 			return null;
 	}
 	
+	
+	public String getBotFilePath() {
+		return botFilePath;
+	}
+
+	public String getUsersName() {
+		return usersName;
+	}
 	
 	
 }
