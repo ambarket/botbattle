8ed005709f09cb1afbfdbd5de725aeb6495a7f77
Randall Hudson <Hudsonrj09@gmail.com>
2015-02-25 22:14:21 -0500

    Updated player.java to a class with a Process field.

--- cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/Player.java
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/Player.java
@@ -1,8 +1,56 @@
 package game.manger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 /**
  * @author Randall
  *
  */
-public interface Player {
-	String getMove(String board);
+public class Player {
+	private String botFilePath;
+	private Process botProcess;
+	private String usersName;
+	
+	/**
+	 * @param botFilePath
+	 * @param botProcess
+	 * @param usersName
+	 */
+	public Player(String botFilePath, String usersName) {
+		this.botFilePath = botFilePath;
+		this.usersName = usersName;
+		
+		try {
+			this.botProcess = Runtime.getRuntime().exec("java " + botFilePath + "/" + usersName);
+		} catch (IOException e) {
+			this.botProcess = null;
+		}
+	}
+	
+	public String getBotFilePath() {
+		return botFilePath;
+	}
+
+	public String getUsersName() {
+		return usersName;
+	}
+	
+	public OutputStream getOutputStream(){
+		if(botProcess != null)
+			return botProcess.getOutputStream();
+		else
+			return null;
+	}
+	
+	public InputStream getInputStream(){
+		if(botProcess != null)
+			return botProcess.getInputStream();
+		else
+			return null;
+	}
+	
+	
+	
 }
