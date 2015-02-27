3e28846e781be5223ec428d8a2aed3febca109ca
Stevenpc3 <Stevenpc3@live.com>
2015-02-27 00:05:26 -0500

    Initial test arena push

---
+++ cmpsc488/botbattleapp/nodejs_applications/driverForTestArena.js
@@ -0,0 +1,8 @@
+/**
+ * The entry point of the application.
+ * Run the InitialConfigurationApp, then upon completion run the BotBattleApp
+ */
+var BotBattleServer = require('./BotBattleServer');
+
+var botBattleAppServer = new BotBattleServer().initAndStartListening(6058);
+require('./testArenaPrototype')(botBattleAppServer);
