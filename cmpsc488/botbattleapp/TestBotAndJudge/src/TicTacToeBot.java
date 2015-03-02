56e410ca03e76035e31129ab63ef0beda616cb76
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-01 23:58:47 -0500

    Moved this out of the game manager package since the students will not be putting their bots in their.

--- cmpsc488/botbattleapp/TestBotAndJudge/src/TicTacToeBot.java
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/TicTacToeBot.java
@@ -1,30 +1,42 @@
-package game.manger;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
 public class TicTacToeBot{
 
 	static boolean temp;
 	
-	public static void main(String[] args){
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException{
 		temp = Boolean.parseBoolean(args[0]);
-				
+		PrintWriter writer = new PrintWriter("the-file-name.txt", "UTF-8");
+		PrintWriter writer2 = new PrintWriter("the-file-name2.txt", "UTF-8");
+		writer2.println("test ");
+		writer2.close(); 
 		Scanner scner = new Scanner(System.in);
 		
 		String board;
 		
 		while(scner.hasNext()){
 			board = scner.nextLine();
-			
+			board = scner.nextLine();
+			writer.println("Board read: " + board);
+			writer.flush();
 			if(board.equals("exit")){
 				break;
 			}
 			
 			String move = getMove(board);
+			writer.println("move sent: " + move);
+			writer.flush();
+			System.out.print(move + "\r\n");
 			System.out.println(move);
+			System.out.flush();
+			writer.println("Done sending move");
 		}
-		
-		
+		writer.println("closeing");
+		writer.close();
 	}
 	
 
