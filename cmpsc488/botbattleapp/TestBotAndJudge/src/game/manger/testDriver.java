189a0b97591bc6d7567e5b78256e7da7bd681f98
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-02 00:03:48 -0500

    add code to test first couple moves of a game where the bots are now their own process.

--- cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/testDriver.java
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/testDriver.java
@@ -1,5 +1,7 @@
 package game.manger;
 
+import java.io.IOException;
+
 /**
  * @author Randall Hudson
  *
@@ -8,31 +10,19 @@ public class testDriver {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+
+		String path = "C:\\Users\\Kitty\\git\\botbattle\\cmpsc488\\botbattleapp\\TestBotAndJudge\\bin";
 		
-		//testHumanVersusBot();
+		
+		Player p1 = new Player(path, "TicTacToeBot");
+		Player p2 = new Player(path, "TicTacToeBot");
+		TicTacToeGame game = new TicTacToeGame();
+		GameManager manager = new GameManager(p1, p2, game);
+		manager.run();
+		
 	}
 	
-//	public static void testBotPlayers(){
-//		TicTacToeGame game = new TicTacToeGame();
-//		TicTacToeBot bot1 = new TicTacToeBot(false);
-//		TicTacToeBot bot2 = new TicTacToeBot(true);
-//		
-//		GameManager foo = new GameManager(bot1, bot2, game);
-//		
-//		foo.run();
-//		System.out.println(game.getBoard());
-//	}
-	
-//	public static void testHumanVersusBot(){
-//		TicTacToeGame game = new TicTacToeGame();
-//		TicTacToeBot bot1 = new TicTacToeBot(false);
-//		HumanPlayer palyer2 = new HumanPlayer();
-//		
-//		GameManager foo = new GameManager(bot1, palyer2, game);
-//		
-//		foo.run();
-//		System.out.println(game.getBoard());
-//	}
 }
