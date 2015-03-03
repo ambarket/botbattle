b8b5b6a019e6f9bed21defbbe36b4c1fcaf1883f
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-03 12:03:45 -0500

    Wrote some more unit tests for Player class

--- cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/PlayerTest.java
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/PlayerTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 public class PlayerTest {
 	String path = "C:\\Users\\Kitty\\git\\botbattle\\cmpsc488\\botbattleapp\\TestBotAndJudge\\bin";
+	
 	@Test
 	public void testPlayer() throws IOException {
 		Player p1 = new Player(path, "TicTacToeBot");
@@ -16,41 +17,47 @@ public class PlayerTest {
 	}
 	
 	@Test
-	public void testPlayer2() throws IOException {
+	public void testPlayerBotProcessNotNull() throws IOException {
 		Player p1 = new Player(path, "TicTacToeBot");
 		
 		assertNotNull(p1.botProcess);
 	}
 	
 	@Test
-	public void testPlayer3() throws IOException {
+	public void testPlayerReadStreamNotNull() throws IOException {
 		Player p1 = new Player(path, "TicTacToeBot");
 		
 		assertNotNull(p1.reader);
 	}
 	
 	@Test
-	public void testPlayer4() throws IOException {
+	public void testPlayerWriteStreamNotNull() throws IOException {
 		Player p1 = new Player(path, "TicTacToeBot");
 		
 		assertNotNull(p1.writer);
 	}
+
 	
 	@Test
-	public void testPlayer5() throws IOException {
+	public void testGetMove() throws IOException, InterruptedException {
 		Player p1 = new Player(path, "TicTacToeBot");
+		TicTacToeGame game = new TicTacToeGame();
 		
-	}
-	
-	@Test
-	public void testGetMove() throws IOException {
-		Player p1 = new Player(path, "TicTacToeBot");
-		
+		String move = p1.getMove(game.getBoard());
+		assertNotNull(move);
 	}
 
 	@Test
-	public void testRun() {
-		fail("Not yet implemented");
+	public void testRun() throws IOException {
+		Player p1 = new Player(path, "TicTacToeBot");
+		TicTacToeGame game = new TicTacToeGame();
+		p1.writer.write(game.getBoard() + "\n");
+		p1.writer.flush();
+		p1.run();
+		
+		assertNotNull(p1.move);
+		
+		//assertEquals("Test\n", p1.move);
 	}
 
 }
