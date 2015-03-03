f084e5dcf3846b50cdfa38228cf907d5f24e678f
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-03 17:00:22 -0500

    updated ignore file and added call to game managers to string

--- cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/testDriver.java
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/testDriver.java
@@ -17,12 +17,13 @@ public class testDriver {
 		String path = "C:\\Users\\Kitty\\git\\botbattle\\cmpsc488\\botbattleapp\\TestBotAndJudge\\bin";
 		
 		
-		Player p1 = new Player(path, "TicTacToeBot");
-		Player p2 = new Player(path, "TicTacToeBot");
+		Player p1 = new Player(path, "TicTacToeBot1");
+		Player p2 = new Player(path, "TicTacToeBot2");
 		TicTacToeGame game = new TicTacToeGame();
 		GameManager manager = new GameManager(p1, p2, game);
 		manager.run();
 		
+		System.out.println("\n" + manager);
 	}
 	
 }
