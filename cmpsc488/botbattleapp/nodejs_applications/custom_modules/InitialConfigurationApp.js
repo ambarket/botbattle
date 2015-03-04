15fa99b68759a18400b7388d81b639e7684dd93c
ambarket <ambarket@gmail.com>
2015-03-04 00:40:11 -0500

    Moved multer into initialConfigApp now only registered for that form submission and sends files to local_storage/init_config_tmp. I decided it makes more sense to manually move files as they are ready during init config than to try to direct them during upload

--- cmpsc488/botbattleapp/nodejs_applications/custom_modules/InitialConfigurationApp.js
+++ cmpsc488/botbattleapp/nodejs_applications/custom_modules/InitialConfigurationApp.js
@@ -186,11 +186,92 @@ function InitialConfigurationApp(initConfigAppServer) {
    * @private
    */
   (function registerInitialConfigurationRoutes() {
-	  initConfigAppServer.addStaticFileRoute('/', paths.static_content.html + 'initialConfiguration.html');
+    initConfigAppServer.addStaticFileRoute('/', paths.static_content.html + 'initialConfiguration.html');
 	  
+
+    // Add multer
+    // This needs pulled out of here
+    // security issues
+    // https://github.com/jpfluger/multer/blob/examples/multer-upload-files-to-different-directories.md
+    var multer = require('multer');
+    initConfigAppServer.addDynamicRoute('post', '/processInitialConfiguration', multer({
+      dest : paths.local_storage.init_config_tmp,
+      limits : {
+        fieldNameSize : 100,
+      // files: 2,
+      // fields: 5
+      // fieldNameSize - integer - Max field name size (Default: 100 bytes)
+      // fieldSize - integer - Max field value size (Default: 1MB)
+      // fields - integer - Max number of non-file fields (Default: Infinity)
+      // fileSize - integer - For multipart forms, the max file size (in bytes)
+      // (Default: Infinity)
+      // files - integer - For multipart forms, the max number of file fields
+      // (Default: Infinity)
+      // parts - integer - For multipart forms, the max number of parts (fields
+      // + files) (Default: Infinity)
+      // headerPairs - integer - For multipart forms, the max number of header
+      // key=>value pairs to parse Default: 2000 (same as node's http).
+      },
+      // putSingleFilesInArray: true, // this needs doen for future compat. but
+      // will break current multiproto.
+      rename : function(fieldname, filename) {
+        return filename;
+      },
+      // changeDest: function(dest, req, res) {
+      // return dest + '/user1';
+      // },
+      onFileUploadStart : function(file, req, res) {
+        console.log(file.fieldname + ' is starting ...');
+        // if (file.originalname == 'virus.exe') return false;
+      },
+      onFileUploadComplete : function(file, req, res) {
+        console.log(file.fieldname + ' uploaded to  ' + file.path);
+        // add logic to check the file fieldname and change save directory and
+        // name based on this
+      },
+      onFileUploadData : function(file, data, req, res) {
+        console.log(data.length + ' of ' + file.fieldname + ' arrived')
+      },
+      onParseStart : function() {
+        console.log('Form parsing started at: ', new Date())
+      },
+      onParseEnd : function(req, next) {
+        console.log('Form parsing completed at: ', new Date());
+
+        // Dont need to do any custom parsing, also don't need half these
+        // options
+        // but leave them for now
+        // usage example: custom body parse
+        // req.body = require('qs').parse(req.body);
+        // console.log("HERE!");
+        // console.log(require.resolve('qs'));
+
+        // call the next middleware
+        next();
+      },
+      onError : function(error, next) {
+        console.log(error)
+        next(error)
+      },
+      onFileSizeLimit : function(file) {
+        console.log('Failed: ', file.originalname)
+        fs.unlink('./' + file.path) // delete the partially written file // set
+                                    // in limit object
+      },
+      onFilesLimit : function() {
+        console.log('Crossed file limit!')
+      },
+      onFieldsLimit : function() {
+        console.log('Crossed fields limit!')
+      },
+      onPartsLimit : function() {
+        console.log('Crossed parts limit!')
+      },
+    }));
+	    
 	// multer needs to be added here for security reasons
     initConfigAppServer.addDynamicRoute('post', '/processInitialConfiguration', function(req, res) {
-      console.log(JSON.stringify(req.body));
+      //console.log(JSON.stringify(req.body));
       var sanitizer=require('sanitizer');
       sanitizedFormData = {
         //database parameters
@@ -204,15 +285,15 @@ function InitialConfigurationApp(initConfigAppServer) {
         adminPassword: sanitizer.sanitize(req.body.adminPassword),
         //game module parameters
         gameName: sanitizer.sanitize(req.body.gameName),
-        gameModule: sanitizer.sanitize(req.body.gameModule),
-        gameRules: sanitizer.sanitize(req.body.gameRules),
+        gameModule: req.files.gameModule,
+        gameRules: req.files.gameRules,
         //tournament parameters
         tournamentName: sanitizer.sanitize(req.body.tournamentName),
-        studentList: sanitizer.sanitize(req.body.studentList),
+        studentList: req.files.studentList,
         tournamentDeadline: sanitizer.sanitize(req.body.tournamentDeadline), 
       };
       console.log(JSON.stringify(sanitizedFormData));
-      console.log(req.files);
+      //console.log(req.files);
 
       executeAllInitialConfigurationTasksInSequence();
     });
--- cmpsc488/botbattleapp/nodejs_applications/custom_modules/ObjectFactory.js
+++ cmpsc488/botbattleapp/nodejs_applications/custom_modules/ObjectFactory.js
@@ -4,6 +4,16 @@ module.exports = {
       return new (function() {
         this.username = username;
         this.password = password;
-      })()
+      })();
+    },
+
+    createGameModuleObject : function(gameName, rulesLocation, classFileLocation, moveTimeout) {
+      return new (function() {
+        this.gameName = gameName;
+        this.rulesLocation = rulesLocation;
+        this.classFileLocation = classFileLocation;
+        this.moveTimeout = moveTimeout;
+      })();
     }
+
 }
