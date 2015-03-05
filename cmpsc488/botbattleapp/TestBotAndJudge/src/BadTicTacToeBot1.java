5c6781e11e344c2fea4738028a45a65924b26d02
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-04 21:34:30 -0500

    created a bad tictactoe bot to better test timeout on getMove function

---
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/BadTicTacToeBot1.java
@@ -0,0 +1,62 @@
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+
+public class BadTicTacToeBot1{
+
+	static boolean temp;
+	
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException{
+		temp = false;
+		PrintWriter writer = new PrintWriter("the-file-name1.txt", "UTF-8");//TODO: remove testing file
+		
+		Scanner scner = new Scanner(System.in);
+		
+		String board;
+		
+		while(scner.hasNextLine()){
+			board = scner.nextLine();
+			
+			writer.println("Board read: " + board);
+			writer.flush();
+			if(board.equals("exit")){
+				break;
+			}
+			
+			String move = getMove(board);
+			writer.println("move sent: " + move);
+			writer.flush();
+			System.out.print(move + "\r\n");
+
+			System.out.flush();
+			writer.println("Done sending move");
+		}
+		writer.println("closeing");
+		writer.close();
+	}
+	
+
+	public static String getMove(String board) {
+		int row = 1, col = 1;
+		int i = 0;
+		for (char c : board.toCharArray()) {
+			if(c == '0'){
+				String move = row + ", " + col + ", " + (temp ? "X" : "O");
+				return move;
+			}
+			
+			col++;
+			if(col == 4){
+				col = 1;
+				row++;
+			}
+		}
+		
+		
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
