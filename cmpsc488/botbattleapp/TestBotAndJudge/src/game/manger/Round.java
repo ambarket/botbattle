cef0c641d68a45a68d14f60adda043a56fadfcca
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-09 22:08:13 -0400

    got a good bit of the tournament and round classes done.

--- cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/Round.java
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/Round.java
@@ -1,5 +1,48 @@
 package game.manger;
 
-public class Round {
+import java.io.IOException;
 
+public class Round {
+	
+	private GameManager games[];
+	
+	public Round(CompetitorData competitors) throws IOException {
+		
+		games = new GameManager[(int) Math.ceil(competitors.getNumberOfCompetitors() / 2)];
+		
+		for (int i = 0; i < games.length; i++) {
+			
+			Player p1 = new Player(competitors.getUser(i), competitors.getBotPath(i));
+			Player p2 = null;
+			
+			if( i + 1 > competitors.getNumberOfCompetitors() ) { //Possible we have odd number of competitors
+				p2 = new Player("badBot", "badBot");// TODO
+			} else {
+				p2 = new Player(competitors.getUser(i + 1), competitors.getBotPath(i + 1));
+			}
+			
+			games[i] = new GameManager(p1, p2);
+		}
+	}
+	
+	public void runRound(){
+		for (GameManager gameManager : games) {
+			gameManager.run(); 
+		}
+	}
+	
+	public CompetitorData getWinners() {
+		return null; //TODO
+	}
+	
+	public String toHTML() { //TODO 
+		String html = "";
+		
+		for (GameManager gameManager : games) {
+			html += gameManager.getHTMLForEntireGame();
+		}
+		
+		return html;
+	}
+	
 }
