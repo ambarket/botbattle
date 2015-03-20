f5e5c55cb844a226fec45b6376e02e68022ffd6c
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-20 18:50:33 -0400

    started writing toJSON string function

--- cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/SaveTheIslandGame.java
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/SaveTheIslandGame.java
@@ -2,7 +2,158 @@ package game.manger;
 
 import java.util.Random;
 
-public class SaveTheIslandGame {
+public class SaveTheIslandGame {	
+	public static String getStartingBoard() {
+		Random rng = new Random();
+		String board = "";
+		
+		for(int i = 0; i < 5; i++) {
+			board += rng.nextInt(6);
+		}
+		board += ";1";
+		
+		for(int i = 0; i < 23; i++) {
+			board += "0";
+		}
+		board += "2;";
+		for(int i = 0; i < 5; i++) {
+			board += rng.nextInt(6);
+		}
+		
+		return board;
+	}
+
+	//TODO the only thing else that is needed is the shuffle command.
+	public static String updateBoard(String move, String board, int player) {
+		String updatedBoard = "";
+		String[] peices;
+		if( move.startsWith("attack") ) {
+			 peices = move.split(";");
+			 int value = Integer.parseInt(peices[1].substring(0, 1));
+			 updatedBoard = Board.executeAttack(board, (player == 1 ? 2 : 1), peices[1].length());
+		}
+		else if( move.startsWith("retreat") ) {
+			peices = move.split(";");
+			 int value = Integer.parseInt(peices[1].substring(0, 1));
+			 updatedBoard = Board.movePlayer(board, player, -value);
+		}
+		
+		return updatedBoard;
+	}
+	
+	public static boolean isGameOver(String board) {
+		//This game doesnt have ties so winning is the only way it will end.
+		if(isGameWon(board)){
+			return true;
+		}
+		
+		return false;
+	}
+
+	public static boolean isGameWon(String board) {
+		String island = Board.getIsland(board);
+		
+		if( island.indexOf("1") != -1 && island.indexOf("2") != -1 ) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public static boolean isValidMove(String move, String board, int player) {
+		int typeOfMove = 0,
+			tilesForMove = 1;
+		String[] peices = move.split(";");
+		
+		if( peices.length != 2 ) {
+			return false;
+		}
+
+		if( !peices[typeOfMove].equals("attack") 
+				&& !peices[typeOfMove].equals("retreat") ) {
+			return false;
+		}
+
+		//Check all given tiles are the same
+		int tileValue = Integer.parseInt(peices[tilesForMove].substring(0, 1));
+		peices[tilesForMove].matches(tileValue + "+");
+
+		//Check player has those tiles
+		if( !Board.checkPlayersTiles(board, player, tileValue, peices[tilesForMove].length()) ) {
+			return false;
+		}
+
+		return true;
+	}
+	
+	
+	public static String getHTMLForBoard(String board) {
+		// TODO implement getHTML for save the island
+		return null;
+	}
+	
+	public static int getBotTimeoutInMilliseconds() {
+		return 3000;
+	}
+	
+	public static String getName() {
+		return "SaveTheIsland";
+	}
+	
+	//TODO implement getJSONstringFromGameResults
+	public static String getJSONstringFromGameResults(GameResults results) {
+		String[] p1Moves = (String[]) results.getPlayer1Moves().toArray();
+		String[] p2Moves = (String[]) results.getPlayer2Moves().toArray();
+		String board = results.getBoards().get(0);
+		
+		String jsonString = "[{";		
+		jsonString += "\'gameData' : {\n";
+		jsonString += "\t\'player1Tiles\' : \"" + Board.getPlayersTiles(1, board) + "\",\n";
+		jsonString += "\t\'player2Tiles\' : \"" + Board.getPlayersTiles(2, board) + "\",\n";
+		jsonString += "\t\'turnDescription\' : \"" + "initial board" + "\"\n}\n";
+		
+		for (int i = 1; i < results.getBoards().size(); i++) {
+			board = results.getBoards().get(i);
+			
+			jsonString += "\'gameData' : {\n";
+			jsonString += "\t\'player1Tiles\' : \"" + Board.getPlayersTiles(1, board) + "\",\n";
+			jsonString += "\t\'player2Tiles\' : \"" + Board.getPlayersTiles(2, board) + "\",\n";
+			
+			
+			if( i % 2 == 1 ) {
+				jsonString += "\t\'turnDescription\' : \"" + p1Moves[i/2] + "\"\n}\n";
+			} else {
+				jsonString += "\t\'turnDescription\' : \"" + p2Moves[i/2] + "\"\n}\n";
+			}
+		}
+		
+		jsonString += "]";
+		
+//		[
+//            {
+//              'animatableEvents': [
+//                 {
+//                   'event': 'move',
+//                   'data': { 
+//                     'objectName' : 'player1',
+//                     'finalPosition' : 9 
+//                   } 
+//                 },
+//              ],
+//              'gameData' : {
+//                'player1Tiles' : [1, 3, 5, 5, 3],
+//                'player2Tiles' : [2, 4, 3, 5, 1],
+//                'turnDescription' : "Player 2 used a 3 tile to move to position 11.", 
+//              },
+//              'debugData' : {
+//                   lines : [ "An array", "of lines output by the bot", "stderr on this turn." ]
+//              },
+//            }
+//	]
+		
+		
+		return jsonString;
+	}
 	
 	//-------------------------- BOARD CLASS ---------------------
 	static class Board {
@@ -95,113 +246,6 @@ public class SaveTheIslandGame {
 		}
 	}
 	//----------------------- END BOARD CLASS ---------------------
-	
-	public static String getStartingBoard() {
-		Random rng = new Random();
-		String board = "";
-		
-		for(int i = 0; i < 5; i++) {
-			board += rng.nextInt(6);
-		}
-		board += ";1";
-		
-		for(int i = 0; i < 23; i++) {
-			board += "0";
-		}
-		board += "2;";
-		for(int i = 0; i < 5; i++) {
-			board += rng.nextInt(6);
-		}
-		
-		return board;
-	}
-	
-	//TODO implement getJSONstringFromGameResults
-	public static String getJSONstringFromGameResults(GameResults results) {
-		return null;
-	}
-
-	//TODO the only thing else that is needed is the shuffle command.
-	public static String updateBoard(String move, String board, int player) {
-		String updatedBoard = "";
-		String[] peices;
-		if( move.startsWith("attack") ) {
-			 peices = move.split(";");
-			 int value = Integer.parseInt(peices[1].substring(0, 1));
-			 updatedBoard = Board.executeAttack(board, (player == 1 ? 2 : 1), peices[1].length());
-		}
-		else if( move.startsWith("retreat") ) {
-			peices = move.split(";");
-			 int value = Integer.parseInt(peices[1].substring(0, 1));
-			 updatedBoard = Board.movePlayer(board, player, -value);
-		}
-		
-		return updatedBoard;
-	}
-	
-	public static boolean isGameOver(String board) {
-		//This game doesnt have ties so winning is the only way it will end.
-		if(isGameWon(board)){
-			return true;
-		}
-		
-		return false;
-	}
-
-	public static boolean isGameWon(String board) {
-		String island = Board.getIsland(board);
-		
-		if( island.indexOf("1") != -1 && island.indexOf("2") != -1 ) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-	
-	public static String getHTMLForBoard(String board) {
-		// TODO implement getHTML for save the island
-		return null;
-	}
-	
-	public static int getBotTimeoutInMilliseconds() {
-		return 3000;
-	}
-	
-	public static String getName() {
-		return "SaveTheIsland";
-	}
-
-	public static boolean isValidMove(String move, String board, int player) {
-		int typeOfMove = 0,
-			tilesForMove = 1;
-		String[] peices = move.split(";");
-		
-		if( peices.length != 2 ) {
-			return false;
-		}
-
-		if( !peices[typeOfMove].equals("attack") 
-				&& !peices[typeOfMove].equals("retreat") ) {
-			return false;
-		}
-
-		//Check all given tiles are the same
-		int tileValue = Integer.parseInt(peices[tilesForMove].substring(0, 1));
-		peices[tilesForMove].matches(tileValue + "+");
-
-		//Check player has those tiles
-		if( !Board.checkPlayersTiles(board, player, tileValue, peices[tilesForMove].length()) ) {
-			return false;
-		}
-
-		return true;
-	}
-	
-	
-	
-	
-	
-	
 	
 	
 	
