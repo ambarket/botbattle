de7d54142a8b81110466bcd6652fc78fcecc9d58
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-22 14:48:20 -0400

    auto formatting and displays results of some of the JSON string functions

--- cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/SaveTheIslandGameTest.java
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/SaveTheIslandGameTest.java
@@ -7,63 +7,70 @@ import org.junit.Test;
 
 public class SaveTheIslandGameTest {
 
-	@Test
-	public void testGetStartingBoardPositionForPlayer1() {
-		String board = SaveTheIslandGame.getStartingBoard();
-		
-		assertEquals("1", board.substring(6, 7));
-	}
-	
-	@Test
-	public void testGetStartingBoardPositionForPlayer2() {
-		String board = SaveTheIslandGame.getStartingBoard();
-		
-		assertEquals("2", board.substring(30, 31));
-	}
+  @Test
+  public void testGetStartingBoardPositionForPlayer1() {
+    String board = SaveTheIslandGame.getStartingBoard();
 
-	@Test
-	public void testUpdateBoard() {
-		fail("Not yet implemented");
-	}
+    assertEquals("1", board.substring(6, 7));
+  }
 
-	@Test
-	public void testIsValidMove() {
-		String board = SaveTheIslandGame.getStartingBoard();
-		String tiles = Board.getPlayersTiles(1, board);
-		String move = "attack;" + tiles.substring(0, 1);
+  @Test
+  public void testGetStartingBoardPositionForPlayer2() {
+    String board = SaveTheIslandGame.getStartingBoard();
 
-		assertTrue(SaveTheIslandGame.isValidMove(move, board, 1));
-	}
-	
-	@Test
-	public void testIsValidMoveWithBadType() {
-		String board = SaveTheIslandGame.getStartingBoard();
-		String tiles = Board.getPlayersTiles(1, board);
-		String move = "ammack;" + tiles.substring(0, 1);
+    assertEquals("2", board.substring(30, 31));
+  }
 
-		assertFalse(SaveTheIslandGame.isValidMove(move, board, 1));
-	}
-	
-	@Test
-	public void testIsValidMoveWithBadTile() {
-		String board = SaveTheIslandGame.getStartingBoard();
-		String tiles = Board.getPlayersTiles(1, board);
-		String move = "attack;6";
+  @Test
+  public void testUpdateBoard() {
+    fail("Not yet implemented");
+  }
 
-		assertFalse(SaveTheIslandGame.isValidMove(move, board, 1));
-	}
-	
-	@Test
-	public void testIsValidMoveWithBadNumberOfTiles() {
-		String board = SaveTheIslandGame.getStartingBoard();
-		String tiles = Board.getPlayersTiles(1, board);
-		String move = "attack;" + tiles.substring(0, 1) + tiles.substring(0, 1) + tiles.substring(0, 1) + tiles.substring(0, 1) + tiles.substring(0, 1) + tiles.substring(0, 1);
+  @Test
+  public void testIsValidMove() {
+    String board = SaveTheIslandGame.getStartingBoard();
+    String tiles = Board.getPlayersTiles(1, board);
+    String move = "attack;" + tiles.substring(0, 1);
 
-		assertFalse(SaveTheIslandGame.isValidMove(move, board, 1));
-	}
+    assertTrue(SaveTheIslandGame.isValidMove(move, board, 1));
+  }
 
-	@Test
-	public void testIsGameOverWithStartingBoard() {
-		assertFalse(SaveTheIslandGame.isGameOver(SaveTheIslandGame.getStartingBoard()));
-	}
+  @Test
+  public void testIsValidMoveWithBadType() {
+    String board = SaveTheIslandGame.getStartingBoard();
+    String tiles = Board.getPlayersTiles(1, board);
+    String move = "ammack;" + tiles.substring(0, 1);
+
+    assertFalse(SaveTheIslandGame.isValidMove(move, board, 1));
+  }
+
+  @Test
+  public void testIsValidMoveWithBadTile() {
+    String board = SaveTheIslandGame.getStartingBoard();
+    String tiles = Board.getPlayersTiles(1, board);
+    String move = "attack;6";
+
+    assertFalse(SaveTheIslandGame.isValidMove(move, board, 1));
+  }
+
+  @Test
+  public void testIsValidMoveWithBadNumberOfTiles() {
+    String board = SaveTheIslandGame.getStartingBoard();
+    String tiles = Board.getPlayersTiles(1, board);
+    String move =
+        "attack;" + tiles.substring(0, 1) + tiles.substring(0, 1) + tiles.substring(0, 1)
+            + tiles.substring(0, 1) + tiles.substring(0, 1) + tiles.substring(0, 1);
+
+    assertFalse(SaveTheIslandGame.isValidMove(move, board, 1));
+  }
+
+  @Test
+  public void testIsGameOverWithStartingBoard() {
+    assertFalse(SaveTheIslandGame.isGameOver(SaveTheIslandGame.getStartingBoard()));
+  }
+
+  @Test
+  public void testAnimatedEventJSON() {
+    //TODO write unit test for JSON functions in game class
+  }
 }
--- cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/testDriver.java
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/testDriver.java
@@ -1,5 +1,7 @@
 package game.manger;
 
+import game.manger.SaveTheIslandGame.Board;
+
 import java.io.IOException;
 
 /**
@@ -24,21 +26,13 @@ public class testDriver {
 	 */
 	public static void main(String[] args) throws IOException {
 
-		TournamentTest();
+	//TournamentTest();
 		
-//		String path = "C:\\Users\\Kitty\\git\\botbattle\\cmpsc488\\botbattleapp\\TestBotAndJudge\\bin";
-//		
-//		
-//		Player p1 = new Player(path, "TicTacToeBot1");
-//		Player p2 = new Player(path, "TicTacToeBot2");
-//		//Player p2 = new Player(path, "BadTicTacToeBot1");
-//
-//		GameManager manager = new GameManager(p1, p2);
-//		manager.run();
-//		
-//		//System.out.println("\n" + manager);
-//		
-//		System.out.println("JSON: " + manager.getJSONStringOfResults());
+	  String s = SaveTheIslandGame.animatedEventJSON("initialSetup", "None", -1);
+	  System.out.println(s);
+	  String b = SaveTheIslandGame.getStartingBoard();
+	  s = SaveTheIslandGame.gameDataJSON(Board.getPlayersTiles(1, b), Board.getPlayersTiles(2, b), "Start");
+	  System.out.println(s);
 	}
 	
 	public static void TournamentTest() {
