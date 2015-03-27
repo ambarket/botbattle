1f89d174afb02df6abc43522d836d1a733822ac4
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-26 23:38:20 -0400

    added test for having a human play against a bot. Right now it just hangs because you need to grab the input and output streams of the humans bot and use those.

--- cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/GameManager.java
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/game/manger/GameManager.java
@@ -7,46 +7,53 @@ import java.io.IOException;
  *
  */
 public class GameManager {
-	
-	//TODO create simple save the island bot
-	//TODO test save the island game with simple bot
-	//TODO write unit tests for rounds
-	//TODO write unit tests for tournaments
-	//TODO write unit tests for Competitor Data
-	//TODO write unit tests for game manager
-	//TODO look into integration testing
-	//TODO decide if we need gameInterface, if so update it. 
-	
-	/**
-	 * @param args
-	 * @throws IOException 
-	 */
-	public static void main(String[] args) throws IOException {
+  
+  //You will need to change this path
+  public static  String path = "C:\\Users\\Kitty\\git\\botbattle\\cmpsc488\\botbattleapp\\TestBotAndJudge\\bin";
 
-	TournamentTest();
-		
-//	  String s = SaveTheIslandGame.animatedEventJSON("initialSetup", "None", -1);
-//	  System.out.println(s);
-//	  String b = SaveTheIslandGame.getStartingBoard();
-//	  s = SaveTheIslandGame.gameDataJSON(Board.getPlayersTiles(1, b), Board.getPlayersTiles(2, b), "Start");
-//	  System.out.println(s);
-	}
-	
-	//This test is for a tic tac toe tournament
-	public static void TournamentTest() {
-		String path = "C:\\Users\\Kitty\\git\\botbattle\\cmpsc488\\botbattleapp\\TestBotAndJudge\\bin";
-		CompetitorData c = new CompetitorData();
-		c.addUser("rvh5220", path);
-		c.addUser("rvh5221", path);
-		c.addUser("rvh5222", path);
-		c.addUser("rvh5223", path);
-		
-		Tournament t = new Tournament(null, null, c);
-		try {
-			t.runTournament();
-		} catch (IOException e) { //TODO this should probablly be caught lower down
-			e.printStackTrace();
-		}
-	}
-	
+  // TODO create simple save the island bot
+  // TODO test save the island game with simple bot
+  // TODO write unit tests for rounds
+  // TODO write unit tests for tournaments
+  // TODO write unit tests for Competitor Data
+  // TODO write unit tests for game manager
+  // TODO look into integration testing
+  // TODO decide if we need gameInterface, if so update it.
+
+  /**
+   * @param args
+   * @throws IOException
+   */
+  public static void main(String[] args) throws IOException {
+
+    //TournamentTest();
+    humanPlayersTest();
+  }
+  
+  public static void humanPlayersTest() throws IOException {
+    System.out.flush();
+    Player p1 = new Player(path);
+    System.out.flush();
+    Player p2 = new Player(path, "rvh5220");
+    
+    GameInstance game = new GameInstance(p1, p2);
+    game.run();
+  }
+
+  // This test is for a tic tac toe tournament
+  public static void TournamentTest() {
+    CompetitorData c = new CompetitorData();
+    c.addUser("rvh5220", path);
+    c.addUser("rvh5221", path);
+    c.addUser("rvh5222", path);
+    c.addUser("rvh5223", path);
+
+    Tournament t = new Tournament(null, null, c);
+    try {
+      t.runTournament();
+    } catch (IOException e) { // TODO this should probablly be caught lower down
+      e.printStackTrace();
+    }
+  }
+
 }
