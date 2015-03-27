a9f2d9c3203ff9a447450ac94fe719eb01eb3f4f
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-26 23:45:19 -0400

    added a sleep call so it doesnt loop forever a lot. If you are reading and writing with the human faster or slower then this u can change it.

--- cmpsc488/botbattleapp/TestBotAndJudge/src/HumanPlayer.java
+++ cmpsc488/botbattleapp/TestBotAndJudge/src/HumanPlayer.java
@@ -9,7 +9,7 @@ public class HumanPlayer {
   static PrintWriter writer;
   static Scanner scner;
   
-  public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+  public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException, InterruptedException {
     writer = new PrintWriter("humanPlayer.txt", "UTF-8");//TODO: remove testing file
     writer.println("Starting");
     writer.flush();
@@ -20,6 +20,9 @@ public class HumanPlayer {
       //No reading or writing should actually be done here
       //It should be done be getting this processes input and output streams
       // and writing/reading to those
+      
+      //Pause for 1 second, cant pause forever otherwise u wouldnt be able to read or write 
+      Thread.sleep(1000);
     }
     
     writer.println("Shutting down");
