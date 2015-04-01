efd983518b500352b8a05205c738f3a7ad280f1c
ambarket <ambarket@gmail.com>
2015-04-01 01:50:15 -0400

    Added categories for BotBattleCompiler and BotBattleApp

--- cmpsc488/botbattleapp/nodejs_applications/custom_modules/Logger.js
+++ cmpsc488/botbattleapp/nodejs_applications/custom_modules/Logger.js
@@ -11,7 +11,9 @@ var categoryDestinations = {
     session : ['ignore'],
     fileManager : ['console'],
     initialConfig : ['console'],
-    httpsServer : ['console']
+    httpsServer : ['console'].
+    BotBattleCompiler : ['console'],
+    BotBattleApp : ['console']
 }
   
 function Logger(destination) {
