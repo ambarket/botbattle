cfc418829b5c2521a42051c654328c3d066339aa
ambarket <ambarket@gmail.com>
2015-04-03 02:22:48 -0400

    Added flashStatusOrErrorMessage(type, message) function to base.js. This should therefore be accessible from all pages in all javascript files. Kept the message stuff from login the same. Tried to change it to work with this function but couldn't get it to work right.

--- cmpsc488/botbattleapp/nodejs_applications/static/javascript/base.js
+++ cmpsc488/botbattleapp/nodejs_applications/static/javascript/base.js
@@ -1,8 +1,19 @@
-/**
- * New node file
- */
-  $('.message').slideDown(function() {
+  function flashStatusOrErrorMessage(type, message) {
+    if (type === 'status') {
+      $('message').html('<p style="color:green>"' + message);
+    }
+    else if (type === 'error') {
+      
+      $('message').html('<p style="color:red>"' + message);
+      console.log(type, message, $('message').html());
+    }
+    else {
+      $('message').html(message);
+    }
+    $('message').slideDown(function() {
+
       setTimeout(function() {
-          $('.message').slideUp();
-      }, 3000);
-  });
+          $('message').slideUp();
+      }, 2000);
+    });
+  }
