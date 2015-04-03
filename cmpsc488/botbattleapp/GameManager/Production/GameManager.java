c50ca5a3ef13365777fd06b3ce609b1072f0800a
ambarket <ambarket@gmail.com>
2015-04-03 19:35:18 -0400

    Added some comments about writing valid JSON that were tripping us up.

--- cmpsc488/botbattleapp/GameManager/Production/GameManager.java
+++ cmpsc488/botbattleapp/GameManager/Production/GameManager.java
@@ -71,6 +71,14 @@ public class GameManager {
 	  //  gameData : an arbitrary game specific object containing necessary information"
 	  //  debugData : an arbitrary game specific object containing necessary information"
 	  // Each animatableEvent must have an event name and data object"
+    
+      /* Lessons about JSON
+       * No single quotes anywhere. You must use escpaed double quotes as down below
+       * No extraneous commas anywhere!
+       * e.g. Commas should be only between two elements in an array or object, not after the last element in that object.
+       * VALID:  { "data" : "move", "data2" : "move2" } 
+       * INVALID { "data" : "move", "data2" : "move2", }  <==PROBLEM is the extra comma after "move2"
+       */
 	  switch(stateNum) {
 	  	case 0:
   			return "{"			 
