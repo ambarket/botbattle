175137769c93c65319d9e3b18332cdca0ba3b35d
Randall Hudson <Hudsonrj09@gmail.com>
2015-04-04 04:31:50 -0400

    changed this to run simple test. It works exactly like it should. Get player1 moves from stdin and sends json of each turn to stdout.

--- cmpsc488/botbattleapp/GameManager/Production/ArenaGameManager.java
+++ cmpsc488/botbattleapp/GameManager/Production/ArenaGameManager.java
@@ -3,20 +3,23 @@
 import java.io.IOException;
 
 public class ArenaGameManager {
-
+  public static  String path = "C:\\Users\\Kitty\\git\\botbattle\\cmpsc488\\botbattleapp\\GameManager\\bin";
   public static void main(String[] args) {
-    if (args.length < 2) {
-      System.err.println("Must supplie arguments of bot path and username.");
-      System.exit(1);
-    }
+//    if (args.length < 2) {
+//      System.err.println("Must supplie arguments of bot path and username.");
+//      System.exit(1);
+//    }
 
-    String botPath = args[0];
-    String username = args[1];
+    //String botPath = args[0];
+    //String username = args[1];
+    String botPath = path;
+    String username = "rvh5220";
+    
     Player bot = null;
 
     System.out.flush();
     try {
-      bot = new Player(botPath, "rvh5220");
+      bot = new Player(botPath, username);
     } catch (IOException e) {
       System.out.println("An Exception was thrown.");
     }
