41f5d1500864c33885d075745d7b4d5cdbb35b05
ambarket <ambarket@gmail.com>
2015-04-07 06:03:12 -0400

    New Game Module folder layout

---
+++ cmpsc488/botbattleapp/nodejs_applications/local_storage/game_modules/SuperAwesomeFunGame/GameManagerSource/GameInterface.java
@@ -0,0 +1,24 @@
+
+
+
+/**
+ * @author Randall Hudson
+ *
+ */
+public interface GameInterface {
+		
+  String getStartingBoard();
+
+  String updateBoard(String move, String board, int player);
+
+  boolean isValidMove(String move, String board, int player);
+
+  boolean isGameOver(String board);
+
+  boolean isGameWon(String board);
+
+  String getHTMLForBoard(String board);
+
+  String getJSONstringFromGameResults(GameResults results);
+
+}
